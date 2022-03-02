8bd251e68aa1ee763f2107c845f0086b7353f032
Marco Zampini <marco@bloom-it.io>
2022-03-02 17:33:36 +0100

    Iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,39 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(callback) {
-    // ... your code goes here
+    // should increment by 1 the currentTime property on every 1 second interval
+    this.intervalId = setInterval(() => this.currentTime++, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    // should return a number without decimal places
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    // should return the seconds portion of the currentTime that remains after removing the minutes
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    //should return '00' when the value is 0
+    return value.toString().padStart(2, '0')
   }
 
   stop() {
-    // ... your code goes here
+    //should call clear the interval
+    //should stop a previously started chronometer
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    //should reset the value of the "currentTime" property to 0
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    //should return valid format with minutes and seconds
+    const twoDigitsMinutes = this.computeTwoDigitNumber(this.getMinutes())
+    const twoDigitsSeconds = this.computeTwoDigitNumber(this.getSeconds())
+    return `${twoDigitsMinutes}:${twoDigitsSeconds}`
   }
 }
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
-  module.exports = Chronometer;
+  module.exports = Chronometer
 }
